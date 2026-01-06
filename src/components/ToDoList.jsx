b3f37f3f306fdff0a852e74dc46e5c202b8941a8
On-KeiShe <on-kei_she@hotmail.co.uk>
2026-01-06 15:08:29 +0000

    Added To Do List

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,6 +1,39 @@
+import { useState } from "react";
+
 function ToDoList() {
-  return <></>;
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  function addTask(event) {
+    event.preventDefault();
+
+    if (task === "") return;
+
+    setTasks([...tasks, task]);
+    setTask("");
+  }
+
+  return (
+    <div>
+      <h2>To Do</h2>
+
+      <form onSubmit={addTask}>
+        <input
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Add a task"
+        />
+
+        <button type="submit">Add</button>
+      </form>
+
+      <ul>
+        {tasks.map((t, index) => (
+          <li key={index}>{t}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ToDoList;
-
