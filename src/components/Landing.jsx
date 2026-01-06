2ae22c856e2161131d3b7667aebac76acb39aaf8
On-KeiShe <on-kei_she@hotmail.co.uk>
2026-01-06 15:34:36 +0000

    Added Landing Page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,5 +1,10 @@
 function Landing() {
-  return <></>;
+  return (
+  <div>
+  <h1 class="heading"> Welcome to Your Travel Organiser</h1>
+  <p class="landing-paragrah">This app helps you plan trips, organise task, and keep everything related to your travel in one simple place.</p>
+  </div>
+  )
 }
 
 export default Landing;
