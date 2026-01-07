bb77f8fd334d3cebfc467b17d7bcea5baf34fc07
On-KeiShe <on-kei_she@hotmail.co.uk>
2026-01-07 19:28:36 +0000

    Added styling to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <>
-      <header>
+    <header className="header">
+      <nav className="nav">
         <Link to="/">Home</Link>
         <Link to="/countdown">Countdown</Link>
         <Link to="/to-do-list">To Do List</Link>
         <Link to="/weather">Weather</Link>
         <Link to="/virtual-postcard">Send A Postcard</Link>
-      </header>
-    </>
+      </nav>
+    </header>
   );
 }
 
