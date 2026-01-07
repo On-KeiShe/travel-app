30cebd0b2afdad61b44bb1fc4d0263d1c506381b
On-KeiShe <on-kei_she@hotmail.co.uk>
2026-01-07 19:47:15 +0000

    Added changes to the landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,11 +1,18 @@
 function Landing() {
   return (
-  <div>
-  <h1 className="heading"> Welcome to Your Travel Organiser</h1>
-  <p className="intro-paragraph">This app helps you plan trips, organise task, and keep everything related to your travel in one simple place.</p>
-  <p className="intro-start">To begin, click on any headings from the menu. Happy Organising 📝</p>
-  </div>
-  )
+    <>
+      <h1 className="heading"> Welcome to Your Travel Organiser</h1>
+      <p className="intro-paragraph">
+        This app helps you plan trips, organise task, and keep everything
+        related to your travel in one simple place.
+      </p>
+      <div className="intro-start-wrapper">
+        <p className="intro-start">
+          To begin, click on any headings from the menu. Happy Organising 📝
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default Landing;
